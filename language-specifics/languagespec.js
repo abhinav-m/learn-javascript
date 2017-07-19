2c99445777f7bda8342fb01810781d188489f30a
Abhinav Mishra <abhinav.thinktank@gmail.com>
2017-07-19 22:49:39 +0530

    Added various closure examples

--- language-specifics/languagespec.js
+++ language-specifics/languagespec.js
@@ -1,6 +1,7 @@
 var globalVar;
 
 $(document).ready(function() {
+//Scope example
 //This variable has global scope (window scope) in browser
 globalVar = "globalScope";
 console.log("currently global scope");
@@ -8,8 +9,43 @@ differentScope();
 var object1 = {};
 var object2 = object1;
 console.log(object1==object2);
+
+
+//Closure examples
+var divByTwo = closureDivideExample(2);
+var divByThree = closureDivideExample(3);
+//Should Print 2 and 3
+console.log(divByTwo(4));
+console.log(divByThree(9));
+//CLOSURE IIFE example
+//This returns incorrectly bounded function to current reference of i
+var exampleObjects = [{prop:10},{prop:5},{prop:6}];
+var setProps = closureWrongWay(exampleObjects);
+var firstProp = setProps[0];
+console.log("Should be 13 "+firstProp.prop());
+var myPrivProp = closurePrivatePropExample();
+//This corrects above problem , and assigns the actual value on each loop iteration by 
+//invoking the function immediately using   Immediately Invoked Function Expression (IIFE)
+var setProps = closureRightWay(exampleObjects);
+var firstProp = setProps[0];
+var secondProp = setProps[1];
+var thirProp = setProps[2];
+
+console.log("Should be 10" + firstProp.prop);
+console.log("Should be 11" + secondProp.prop);
+console.log("Should be 12" + thirProp.prop);
+
+//Example on making properties private using closure.
+//Should reference inner property value
+console.log("Should be 10 "+ myPrivProp.getProp());
+myPrivProp.setProp(11);
+console.log("Should be 11 "+ myPrivProp.getProp());
+
+//Optional arguments example
+
 optionalArguments(1,2);
 optionalArgumentsExp(2);
+
 testGlobal();
 });
 
@@ -71,12 +107,67 @@ console.log("Prints square if provided with no argument, otherwise can be expone
 //This property of Javascript is called closure. Local variables are created for every instance of function called and not "trampled over"
 function closureDivideExample(number)
 {
+	console.log("Value of frozen variable number = "+number);
 	var fn =	function divBy(num) {
-		return =num/number;
+		return num/number;
 	}
 
 	return fn;
 }
+
+
+function closurePrivatePropExample(){
+	var privProp = 10;
+
+	 // We are returning an object with some inner functions​
+    // All the inner functions have access to the outer function's variables​
+
+	return {
+		// This inner function will return the UPDATED privProp variable​
+        // It will return the current value of privProp, even after the setID function changes it
+		getProp: function () {
+			return privProp;
+		},
+
+		setProp: function(value) {
+			privProp = value;	
+		}
+	}
+
+
+}
+
+
+function closureWrongWay(objects) {
+	var i;
+	var myNum = 10;
+	//Each object's property is being assigned myNum+i
+	//However since the function will be called when the closure get's initialized and used, this will return wrong value.
+	//Value of i will be 3 (last value in loop as loop gets executed in outer function call.) when this function is actually used.
+	for(i=0;i<objects.length;i++)
+		objects[i]["prop"] = function() {
+			return myNum + i;
+		}
+
+		return objects;
+}
+
+function closureRightWay(objects) {
+	var i;
+	var myNum = 10;
+	//Here we use  Immediately Invoked Function Expression (IIFE) to solve this shortcoming
+	for(i=0;i<objects.length;i++)
+	{
+		objects[i]["prop"] = function(j) {
+			return function (){
+			return myNum+j;
+		}()
+	  }(i);//By adding() at the end of the function we execute it immediately and return value of myNum +j, instead of returning function.
+	}//Immediately invoke the function with current value of i passed to j after each iteration therefore correcting it.
+	return objects;
+}
+
+
 
 
 
