4123f2154f058b467b81de8e5bb6c8e62ff85407
Abhinav Mishra <abhinav.thinktank@gmail.com>
2017-08-12 21:26:02 +0530

    added an example and some kata

--- language-specifics/promise-example.js
+++ language-specifics/promise-example.js
@@ -4,15 +4,11 @@ function getValue() {
     const allURL = "https://fcctop100.herokuapp.com/api/fccusers/top/alltime";
     const recentURL = "https://fcctop100.herokuapp.com/api/fccusers/top/recent";
     fetch(allURL)
-        .then((r) => {
-            if (r.ok) {
-                console.log(r);
-                return r.blob();
-            }
-            throw new Error("Invalid network response.")
-        })
-        .then(function(b) {
-                console.log(b);
-                fetch(recentURL)
-            }
-        }
+        .then(r => r.ok ? r.json() : new Error("Invalid response"))
+        .then((r) => document.getElementById("data").innerHTML = JSON.stringify(r))
+
+    fetch(recentURL)
+        .then(r => r.ok ? r.json() : new Error("Invalid response"))
+        .then((r) => document.getElementById("data").innerHTML = JSON.stringify(r))
+
+}
