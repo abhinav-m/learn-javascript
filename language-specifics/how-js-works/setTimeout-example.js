d3ffa50aa916a5e0a5b7f2bd7fd083aea12d832b
Abhinav Mishra <abhinav.thinktank@gmail.com>
2017-09-12 16:09:15 +0530

    Added some comments

--- language-specifics/how-js-works/setTimeout-example.js
+++ language-specifics/how-js-works/setTimeout-example.js
@@ -1,8 +1,9 @@
 /* FML.
 This will print 11000 
 as the for loop executes almost immediately, 
-and the last value of i is considered for setTimeOut.
+and i refers to the last value in the loop.
 
+IF LET is used, the value gets assigned to each block of i individually.
 
         */
 for (var i = 1000; i <= 10000; i += 1000) {
