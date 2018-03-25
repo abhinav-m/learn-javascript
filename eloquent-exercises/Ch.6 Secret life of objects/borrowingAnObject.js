6a0e304186679a35da4b5e170dab4dddde0763ce
Abhinav Mishra <abhinav.thinktank@gmail.com>
2018-03-25 19:08:28 +0530

    Added question and second solution

--- eloquent-exercises/Ch.6 Secret life of objects/borrowingAnObject.js
+++ eloquent-exercises/Ch.6 Secret life of objects/borrowingAnObject.js
@@ -1,9 +1,12 @@
+/* Can you think of a way to call hasOwnProperty on an object that has its own
+property by that name? */
+
 let map = {
   one: true,
   two: true,
   hasOwnProperty: true
 };
 
-Object.prototype.hasOwnProperty.call(map, "one");
 
-console.log(Object.prototype.hasOwnProperty.call(map, "one"));
+console.log(Object.prototype.hasOwnProperty.call(map, "three"));
+console.log(Object.prototype.hasOwnProperty.apply(map, ["two"]));
