85d18e6535be9fd54c0ec7c154c21d955c53cc5e
Abhinav Mishra <abhinav.thinktank@gmail.com>
2018-04-06 15:50:04 +0530

    Added an example on async await

--- language-specifics/scopes_and_async/scopes_and_async.js
+++ language-specifics/scopes_and_async/scopes_and_async.js
@@ -62,35 +62,57 @@
    create a closure, and bind each 
    variable (declared using var)
    in the loop iteration.
-*/
-for (var j = 1; j <= 5; j++) {
-  (function(j) {
-    setTimeout(function() {
-      console.log(j);
-    }, j * 1000);
-  })(j);
-}
-
-//Using an anonymous function for IIFE , arrow function inside.
-for (var j = 1; j <= 5; j++) {
-  (function(j) {
-    setTimeout(() => {
-      console.log(j);
-    }, j * 1000);
-  })(j);
-}
-
-//Using an arrow function for IIFE, and arrow function inside.
-for (var j = 1; j <= 5; j++) {
-  (j => {
-    setTimeout(() => {
-      console.log(j);
-    }, j * 1000);
-  })(j);
-}
-
+// */
 // for (var j = 1; j <= 5; j++) {
-//   setTimeout(j => {
-//     console.log(j);
-//   }, j * 1000);
+//   (function(j) {
+//     setTimeout(function() {
+//       console.log(j);
+//     }, j * 1000);
+//   })(j);
 // }
+
+// //Using an anonymous function for IIFE , arrow function inside.
+// for (var j = 1; j <= 5; j++) {
+//   (function(j) {
+//     setTimeout(() => {
+//       console.log(j);
+//     }, j * 1000);
+//   })(j);
+// }
+
+// //Using an arrow function for IIFE, and arrow function inside.
+// for (var j = 1; j <= 5; j++) {
+//   (j => {
+//     setTimeout(() => {
+//       console.log(j);
+//     }, j * 1000);
+//   })(j);
+//}
+
+/* Case 5:
+   Using an async function and await with promises 
+   to serialize operations(make them run one 
+   after another) 
+
+*/
+
+//TODO: Read more about this.
+// Note here we dont have to multiply
+// 1000 by i  as each promise is waited upon
+// to be fulfilled due to async /await (generators)
+function simulateAsync(i) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(i);
+    }, 1000);
+  });
+}
+//Each value is awaited and runs after 1 second.
+async function loop() {
+  for (var i = 1; i <= 5; i++) {
+    let returned = await simulateAsync(i);
+    console.log(returned);
+  }
+}
+
+loop();
