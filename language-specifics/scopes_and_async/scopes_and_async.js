3b3594770423649b49f56ee775e600248c98a095
Abhinav Mishra <abhinav.thinktank@gmail.com>
2018-04-06 13:59:29 +0530

    Added an example of using async calls with scopes.

---
+++ language-specifics/scopes_and_async/scopes_and_async.js
@@ -0,0 +1,96 @@
+/* Case 1 : closure formed
+    with each value of i 
+    output is correct due to closure.
+*/
+// function asyncTask(value) {
+//   setTimeout(() => {
+//     console.log(value);
+//   }, value * 1000);
+// }
+
+// for (var i = 1; i <= 5; i++) {
+//   asyncTask(i);
+// }
+
+/* Case 2: 
+    setTimeout is called directly
+    simulating an async operation
+    with the reference of var variable
+    'j' j will have reference to it's 
+    last value when loop exits ie,6
+*/
+// for (var j = 1; j <= 5; j++) {
+//   setTimeout(function() {
+//     console.log(j);
+//   }, j * 1000);
+// }
+
+// //Note this is the same with an arrow function, since the variable
+// //is declared with the var keyword.
+// for (var j = 1; j <= 5; j++) {
+//   setTimeout(() => {
+//     console.log(j);
+//   }, j * 1000);
+// }
+
+/* Case 3:
+    setTimeout is called with 
+    variable declared by using let
+    simulating async operation,
+    let is block scoped, thus each
+    iteration causes a new value of 
+    variable to be referenced from inside 
+    setTimeout function.
+
+*/
+
+// for (let j = 1; j <= 5; j++) {
+//   setTimeout(function() {
+//     console.log(j);
+//   }, j * 1000);
+// }
+
+//Same with arrow function inside setTimeout
+// for (let j = 1; j <= 5; j++) {
+//   setTimeout(() => {
+//     console.log(j);
+//   }, j * 1000);
+// }
+
+/* Case 4: 
+   Wrapping setTimeout in an IIFE to 
+   create a closure, and bind each 
+   variable (declared using var)
+   in the loop iteration.
+*/
+for (var j = 1; j <= 5; j++) {
+  (function(j) {
+    setTimeout(function() {
+      console.log(j);
+    }, j * 1000);
+  })(j);
+}
+
+//Using an anonymous function for IIFE , arrow function inside.
+for (var j = 1; j <= 5; j++) {
+  (function(j) {
+    setTimeout(() => {
+      console.log(j);
+    }, j * 1000);
+  })(j);
+}
+
+//Using an arrow function for IIFE, and arrow function inside.
+for (var j = 1; j <= 5; j++) {
+  (j => {
+    setTimeout(() => {
+      console.log(j);
+    }, j * 1000);
+  })(j);
+}
+
+// for (var j = 1; j <= 5; j++) {
+//   setTimeout(j => {
+//     console.log(j);
+//   }, j * 1000);
+// }
