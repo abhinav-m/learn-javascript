8f1ce5c166dae387b2d3f797913718baad6b8a40
Abhinav Mishra <abhinav.thinktank@gmail.com>
2018-04-10 23:23:20 +0530

    Added regular expression examples

--- eloquent-sandbox/ch5_regular_expressions/regex_examples.js
+++ eloquent-sandbox/ch5_regular_expressions/regex_examples.js
@@ -244,3 +244,46 @@ let matchPattern_2 = pattern.exec('hyena');
 
 //the regex object.lastIndex will be reset to 0.
 console.log(pattern_2.lastIndex);
+
+/* 
+
+/abc/
+A sequence of characters
+/[abc]/
+Any character from a set of characters
+/[^abc]/ Any character not in a set of characters
+/[0-9]/
+Any character in a range of characters
+/x+/
+One or more occurrences of the pattern x
+/x+?/
+*/
+
+// One or more occurrences, nongreedy
+// /x*/
+
+/* 
+Zero or more occurrences
+/x?/
+Zero or one occurrence
+/x{2,4}/ Between two and four occurrences
+/(abc)/
+A group
+/a|b|c/
+Any one of several patterns
+/\d/
+Any digit character
+/\w/
+An alphanumeric character (“word character”)
+/\s/
+Any whitespace character
+/./
+Any character except newlines
+/\b/
+A word boundary
+/^/
+Start of input
+/$/
+End of input
+
+*/
