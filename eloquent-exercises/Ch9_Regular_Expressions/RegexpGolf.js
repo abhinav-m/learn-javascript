5ef44445780c572aebd644eb1513ed8a64406f83
Abhinav Mishra <abhinav.thinktank@gmail.com>
2018-04-12 23:11:35 +0530

    Added eloquent exercises

--- eloquent-exercises/Ch9_Regular_Expressions/RegexpGolf.js
+++ eloquent-exercises/Ch9_Regular_Expressions/RegexpGolf.js
@@ -19,7 +19,7 @@ smaller.
 //car and cat
 let pattern_1 = /car|t/;
 //can also be /ca[rt]/
-
+console.log('car and cat');
 console.log(pattern_1.test('catandcar'));
 
 //pop and prop
@@ -27,18 +27,49 @@ let pattern_2 = /pr?op/;
 
 let cases = ['he pop', 'prop in react', 'hip hop', 'k pop'];
 
+console.log('prop or pop');
 verify(pattern_2, cases);
 
+//ferry ferrari ferret
 let pattern_3 = /ferr(et|y|ari)/;
 
+console.log('ferrari ferry ferret');
 verify(pattern_3, ['ferrari', 'ferry', 'ferret', 'ferrero']);
 
+//any word ending in ious
 let pattern_4 = /[a-z]ious/;
 
+console.log('spacious', 'any word ending in ious');
 verify(pattern_4, ['spacious ', 'luxurious', 'typhus']);
+
 //Eloquent solution -> /ious\b/
 function verify(pattern, cases) {
   for (let str of cases) {
     console.log(`The pattern matched -> ${pattern.test(str)}`);
   }
 }
+
+//A whitespace character followed by a period, comma, colon, or semicolon
+
+console.log(
+  'A whitespace character followed by a period, comma, colon, or semicolon'
+);
+
+let pattern_5 = / (.|,|:|;|)/;
+//Eloquent solution -> /\s [.|,|:|;|/;
+verify(pattern_5, ['ez .', 'hello :', 'Test ;', 'No;', 'False.', 'Incorrect:']);
+
+//A word longer than six letters
+let pattern_6 = /\w{7,}/;
+
+//A whitespace character followed by a period, comma, colon, or semicolon
+verify(pattern_6, ['tottenham', 'arsenal', '12345678', '123456']);
+
+//A word without the letter e
+console.log('A word without the letter e');
+let pattern_7 = /\b[^\We]+\b/i;
+verify(
+  pattern_7,
+  ['red platypus', 'wobbling nest'],
+  ['earth bed', 'learning ape', 'BEET']
+);
