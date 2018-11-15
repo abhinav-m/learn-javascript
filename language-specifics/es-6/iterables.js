cfa1423fcab726916440448f73aff844e3538689
Abhinav Mishra <abhinav.thinktank@gmail.com>
2018-11-15 10:32:49 +0530

    Added examples on iterables

--- language-specifics/es-6/iterables.js
+++ language-specifics/es-6/iterables.js
@@ -30,3 +30,41 @@ for(let value of iterableObject){
 
 //We can also use it with the spread syntax.
 console.log([...iterableObject])
+
+
+/* 
+yield* is an expression
+that iterates over the operand and
+yields EACH value returned by it.
+The value of yield* expression itself is the value returned 
+by that iterator when it's closed (i.e., when done is true).
+*/
+
+function* generatorOne() {
+    yield 2;
+    yield 3;
+    yield 4;
+}
+
+
+function* generatorTwo() {
+    yield 1;
+    //Delegates the iterator returned from generatorOne to be run here.
+    yield* generatorOne();
+    yield 5;
+    yield 6; 
+}
+
+let iteratorTwo = generatorTwo();
+let iteratorThree = generatorTwo();
+for(value of iteratorTwo){
+    console.log(value);
+}
+
+console.log(iteratorThree.next())
+console.log(iteratorThree.next())
+console.log(iteratorThree.next())
+console.log(iteratorThree.next())
+console.log(iteratorThree.next())
+console.log(iteratorThree.next())
+console.log(iteratorThree.next())
